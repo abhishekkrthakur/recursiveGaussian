977e285e9f87bffac34af242b4f1fe8c0dd8cde8
Abhishek Kumar Thakur <abhishek@extern-251-14.vpn.uni-bonn.de>
2013-01-07 16:38:43 +0100

    Initial Commit

---
+++ recursiveGaussian/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  recursiveGaussian
+//
+//  Created by Abhishek Kumar Thakur on 1/7/13.
+//  Copyright (c) 2013 Abhishek Kumar Thakur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
